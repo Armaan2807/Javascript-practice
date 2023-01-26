3fba4f3bddbc1a6bb53b9a0a40b22b72133a0e99
mohit2314 <mohitpatel2314@gmail.com>
2023-01-26 22:39:53 +0530

    loop2

---
+++ loop2.js
@@ -0,0 +1,13 @@
+// Code for finding even numbers
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+// i=0, 0<10 -> 0 %2 =0 -> print i->0
+// i=1, 1<10 -> 1%2 not equal to 0 -> nothing will print
+// i=2 , 2<10 -> 2%2 =0 -> print -> 2
+// i=3, 3< 10 -> 3%2 =1 -> !=0 -> will not  go inside if condition nothing will be printed
+// i=4; 4<10 -> 4%2 =0 -> print 4
+// i=5;
