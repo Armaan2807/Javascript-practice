2ad24a8ad6392bdd0c9b29cac1abfb5e27e41b01
mohit2314 <mohitpatel2314@gmail.com>
2023-01-26 22:30:09 +0530

    table using loops

--- loop1.js
+++ loop1.js
@@ -29,3 +29,31 @@ console.log("Original Array", arr);
 for (let i = 0; i < arr.length; i++) {
   console.log((arr[i] = arr[i] + 1));
 }
+
+//--------------------------------------------DAY-2 Loops---------------------------------------------
+// for (let i=50; i>4; i=i-5){
+//     console.log(i)
+// }
+//i=50 , i=45
+//40,
+//35>4 -> 35
+// 35-5 =30 , 30>4 -> 30
+// 30-5=25, 25>4 -> 25
+// 25-5=20, 20>4 -> 20
+// 20-5=15 , 15>4 -> 15
+// 15-5=10, 10>4 ->10
+// 10-5=5, 5>4 -> 5
+// 5-5=0, 0  is not greater than 4 -> it will not go inside the loop or we can
+// loop will stop over here
+
+// ************* TABLE OF 3 Using loops *********************
+for (let i = 1; i <= 10; i++) {
+  console.log(3 * i);
+}
+// i=1 , 1<=10 -> 3*1=3
+// i=2, 2<=10 -> 3*2=6
+// i=3, 3<=10 -> 3*3=9
+// i=4; 4<=10 -> 3*4=12
+//................
+// i=10; 10<=10 ->3*10=30
+// i=11; 11<=10 NO -> Loop will stop over here
