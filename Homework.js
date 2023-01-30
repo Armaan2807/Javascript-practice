68068bebfa180b70b04b6d1e1fd998c688660258
armaan <emiratestrader@gmail.com>
2023-01-30 22:26:55 +0530

    COMIITED SOMETHING

--- Homework.js
+++ Homework.js
@@ -1,3 +1,4 @@
 import { bikeObject } from "./Object-1.js";
 
 console.log("Bike object in homework.js", bikeObject);
+// 
--- loop2.js
+++ loop2.js
@@ -1,13 +1,23 @@
 // Code for finding even numbers
+console.log("Code of even numbers")
 for (let i = 0; i < 10; i++) {
   if (i % 2 == 0) {
     console.log(i);
   }
 }
-
 // i=0, 0<10 -> 0 %2 =0 -> print i->0
 // i=1, 1<10 -> 1%2 not equal to 0 -> nothing will print
 // i=2 , 2<10 -> 2%2 =0 -> print -> 2
 // i=3, 3< 10 -> 3%2 =1 -> !=0 -> will not  go inside if condition nothing will be printed
 // i=4; 4<10 -> 4%2 =0 -> print 4
 // i=5;
+console.log("Table of 4")
+for(let i = 1;i<=10;i++) {
+console.log(4*i);
+}
+console.log("Table for odd numbers")
+for (let i=0;i<20;i++){
+  if(i%2!=0){
+console.log(i);
+}
+}
